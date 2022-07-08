0d36258cc479b3a86fa7ee8105c56c4ad24697c9
Lingling Peng <lpeng@ip-10-5-19-81.ec2.internal>
2022-07-08 17:50:27 +0000

    update module3 and instruction link

--- scoring_harness/messages.py
+++ scoring_harness/messages.py
@@ -16,7 +16,7 @@ dry_run = False
 
 ## Edit these URLs to point to your challenge and its support forum
 defaults = dict(
-    challenge_instructions_url = "https://www.synapse.org/#!Synapse:syn12105406/wiki/527865",
+    challenge_instructions_url = "https://www.synapse.org/#!Synapse:syn29616137/wiki/617443",
     support_forum_url = "https://www.synapse.org/#!Synapse:syn29616137/discussion/default",
     scoring_script = "CSBC PS-ON mini-DREAM admins")
 
