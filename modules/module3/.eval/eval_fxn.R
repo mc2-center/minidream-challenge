c859d99e0c5e8fcdf0a584d3c25e141f0a8e290a
Lingling Peng <lpeng@ip-10-5-19-81.ec2.internal>
2022-07-11 14:29:13 +0000

    edit the evaluation script to update answers

--- modules/module3/.eval/eval_fxn.R
+++ modules/module3/.eval/eval_fxn.R
@@ -8,38 +8,38 @@ score_submission <- function(submission_filename) {
   answers <- yaml.load_file(submission_filename)
   
   determinant_guess <- answers$determinant_n
-  dist_euclidean_guess <- answers$dist_euclidean
-  dist_canberra_guess <- format(round(answers$dist_canberra, 2), nsmall = 2)
+  dist_euclidean_guess <- round(answers$dist_euclidean, digits=0) # round to a whole number
+  dist_canberra_guess <- format(round(answers$dist_canberra, 2), nsmall = 2) #keep two digits
   cluster1_guess <- answers$cluster1
   cluster2_guess <- answers$cluster2
   cluster3_guess <- answers$cluster3
 
 
   determinant_actual <- as.integer(0)
-  dist_euclidean_actual <- as.integer(294)
+  dist_euclidean_actual <- as.integer(219)
   dist_canberra_actual <- as.numeric(1.92)
 
   cluster1_actual <- as.integer(1)
   cluster2_actual <- as.integer(2)
-  cluster3_actual <- as.integer(3)
+  cluster3_actual <- as.integer(2)
 
 
   if (determinant_guess == determinant_actual){
     determinant_msg <- "Nailed it!"
   } else {
-    determinant_msg <- "Your answer is incorrect"
+    determinant_msg <- "Your answer was incorrect"
   }
 
   if (dist_euclidean_guess == dist_euclidean_actual){
     dist_euclidean_msg <- "Nailed it!"
   } else {
-    dist_euclidean_msg <- "Your answer is incorrect"
+    dist_euclidean_msg <- "Your answer was incorrect"
   }
 
   if (dist_canberra_guess == dist_canberra_actual){
     dist_canberra_msg <- "Nailed it!"
   } else {
-    dist_canberra_msg <- "Your answer is incorrect"
+    dist_canberra_msg <- "Your answer was incorrect"
   }
 
   if (is.null(cluster1_guess)){
@@ -48,7 +48,7 @@ score_submission <- function(submission_filename) {
       if (cluster1_guess == cluster1_actual){
         cluster1_msg <- "Nailed it!"
       } else {
-        cluster1_msg <- "Your answer is incorrect"
+        cluster1_msg <- "Your answer was incorrect"
       }
   }
 
@@ -58,7 +58,7 @@ score_submission <- function(submission_filename) {
       if (cluster2_guess == cluster2_actual){
         cluster2_msg <- "Nailed it!"
       } else {
-        cluster2_msg <- "Your answer is incorrect"
+        cluster2_msg <- "Your answer was incorrect"
       }
   }
 
@@ -68,7 +68,7 @@ score_submission <- function(submission_filename) {
       if (cluster3_guess == cluster3_actual){
         cluster3_msg <- "Nailed it!"
         } else {
-        cluster3_msg <- "Your answer is incorrect"
+        cluster3_msg <- "Your answer was incorrect"
         }
   }
 
