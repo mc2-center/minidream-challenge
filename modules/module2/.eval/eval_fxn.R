5808de37632231770b0fade4e6bada93c58f3d51
verena <9377970+vpchung@users.noreply.github.com>
2023-06-15 22:42:24 -0700

    update eval fxn

--- modules/module2/.eval/eval_fxn.R
+++ modules/module2/.eval/eval_fxn.R
@@ -1,21 +1,29 @@
 library(yaml)
-library(tidyverse)
-library(lubridate)
-library(glue)
+# library(tidyverse)
+# library(lubridate)
+# library(glue)
 
+check_answer <- function(pred, key) {
+  diff <- round(pred) - round(key)
+  if (diff < 1) {
+    return("Your mean expression level is too low.")
+  } else if (diff > 1) {
+    return("Your mean expression level is too high.")
+  } else {
+    return("Nailed it!")
+  }
+}
 
 score_submission <- function(submission_filename) {
   answers <- yaml.load_file(submission_filename)
   
-  gene_fast <- answers$gene_fast
-  gene_slow <- answers$gene_slow
-  reason_slow <- answers$reason_slow
-  reason_fast <- answers$reason_fast
+  esr1_msg <- check_answer(answers$esr1, key=10509)
+  erbb2_msg <- check_answer(answers$erbb2, key=18198)
+  skew_msg <- ifelse(answers$skew == "positive", "Yes, ", "No, ")
   
-  answers["gene_fast"] <- gene_fast
-  answers["gene_slow"] <- gene_slow
-  answers["reason_slow"] <- reason_slow
-  answers["reason_fast"] <- reason_fast
+  answers["esr1_comment"] <- esr1_msg
+  answers["erbb2_comment"] <- erbb2_msg
+  answers["skew_comment"] <- paste0(skew_msg, "the data is positively skewed.")
 
   answers
 }
